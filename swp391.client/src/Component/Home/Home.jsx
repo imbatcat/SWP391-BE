67d11b4a7b70e7b1afa61f43e4ba546b2e741bab
MinhLuong30 <minhltse182480@fpt.edu.vn>
2024-05-28 11:30:03 +0700

    Home and Login

---
+++ swp391.client/src/Component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar1 from '../NavBar/NavBar'
+import Hero from '../Hero/Hero'
+function Home () {
+  return (
+    <div>
+        <NavBar1/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
---
+++ swp391.client/src/Component/Login/Login.jsx
@@ -0,0 +1,60 @@
+
+import { Link } from 'react-router-dom';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+}
+from 'mdb-react-ui-kit';
+
+function Login() {
+  return (
+    <MDBContainer className="my-5">
+
+      <MDBCard>
+        <MDBRow className='g-0'>
+
+          <MDBCol md='6'>
+            <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp' alt="login form" className='rounded-start w-100'/>
+          </MDBCol>
+
+          <MDBCol md='6'>
+            <MDBCardBody className='d-flex flex-column'>
+
+              <h5 className="fw-bold my-5 pb-2" style={{letterSpacing: '1px', textAlign:'center', fontSize:'30px'}}>Sign into your account</h5>
+
+                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Login</MDBBtn>
+              <a className="small text-muted" style={{textAlign:'end'}} href="#!">Forgot password?</a>
+              <p className="mb-5 pb-lg-2" style={{color: 'Black'}}>Don't have an account? <a href="#!" style={{color: '#393f81'}}>Register here</a></p>
+
+              <div className='d-flex flex-row justify-content-start'>
+                <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                <a href="#!" className="small text-muted">Privacy policy</a>
+              </div>
+
+
+              <div className='d-flex flex-row mt-2' style={{justifyContent:'end'}}>
+                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219'}}/>
+               <Link to="/"> <span className="h1 fw-bold mb-0" style={{fontSize:'20px', color:'black'}}>BACK</span></Link>
+              </div>
+
+            </MDBCardBody>
+          </MDBCol>
+
+        </MDBRow>
+      </MDBCard>
+
+    </MDBContainer>
+  );
+}
+
+export default Login;
