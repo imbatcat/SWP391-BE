af2838a66dcd23b0e178b04c9d4959be9e89ad6b
MinhLuong30 <minhltse182480@fpt.edu.vn>
2024-06-07 11:13:19 +0700

    AccountAdmin

---
+++ swp391.client/src/Pages/Admin Pages/AccountData.jsx
@@ -0,0 +1,13 @@
+export const Accounts=[
+    {
+        fullname: 'Luong The Minh',
+        email: 'minhltse182480@fpt.edu.vn',
+        username: 'MinhLuong',
+        password:'12345',
+        isMale: true,
+        roleId: 0,
+        phonennumber:'0703308389',
+        dob:'30/12/2004'
+
+    }
+]
--- swp391.client/src/Pages/Admin Pages/adminAccount.jsx
+++ swp391.client/src/Pages/Admin Pages/adminAccount.jsx
@@ -1,12 +1,147 @@
 
+import { MDBBadge, MDBBtn, MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit'
 import SideNav from '../../Component/SideNav/SideNav'
+import { useState } from 'react';
 
-function adminAccount() {
+function AdminAccount() {
+  const [accounts, setAccounts] = useState([]);
+  async function fetchData() {
+    try {
+      const response = await fetch('https://localhost:7206/api/Accounts', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'include',
+        headers: {
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        // body data type must match "Content-Type" header
+      });
+      if (!response.ok) {
+        throw new Error("Error fetching data");
+      }
+      const Accounts = await response.json();
+      setAccounts(Accounts); // Ensure setAccounts is correctly referenced
+      console.log(Accounts);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
   return (
     <div>
       <SideNav/>
+      <MDBTable align='middle'>
+      <MDBTableHead>
+        <tr>
+          <th scope='col'>Name</th>
+          <th scope='col'>Title</th>
+          <th scope='col'>Status</th>
+          <th scope='col'>Position</th>
+          <th scope='col'>Actions</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+      {accounts.map((Acc) => (
+        <tr key={Acc.id}>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>{Acc.fullname}</p>
+                <p className='text-muted mb-0'>{Acc.email}</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className='fw-normal mb-1'>{Acc.phonennumber}</p>
+            <p className='text-muted mb-0'>IT department</p>
+          </td>
+          <td>
+            <MDBBadge color='success' pill>
+              Active
+            </MDBBadge>
+          </td>
+          <td>Senior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr>
+        
+      ))}
+        
+        {/* <tr>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/6.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>Alex Ray</p>
+                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className='fw-normal mb-1'>Consultant</p>
+            <p className='text-muted mb-0'>Finance</p>
+          </td>
+          <td>
+            <MDBBadge color='primary' pill>
+              Onboarding
+            </MDBBadge>
+          </td>
+          <td>Junior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/7.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>Kate Hunington</p>
+                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className='fw-normal mb-1'>Designer</p>
+            <p className='text-muted mb-0'>UI/UX</p>
+          </td>
+          <td>
+            <MDBBadge color='warning' pill>
+              Awaiting
+            </MDBBadge>
+          </td>
+          <td>Senior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr> */}
+      </MDBTableBody>
+    </MDBTable>
     </div>
+    
   )
 }
 
-export default adminAccount
+export default AdminAccount
