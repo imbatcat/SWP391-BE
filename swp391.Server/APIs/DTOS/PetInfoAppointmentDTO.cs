5f7fa3bdd0d538eaa5841f6989d458b013162735
Tom <ptdat04@gmail.com>
2024-06-12 08:20:04 +0700

    Added: Pet Api - get simple pet info by appintment id

---
+++ swp391.Server/APIs/DTOS/PetInfoAppointmentDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace PetHealthcare.Server.APIs.DTOS
+{
+    public class PetInfoAppointmentDTO
+    {
+        public string PetName { get; set; }
+    }
+}
--- swp391.Server/Program.cs
+++ swp391.Server/Program.cs
@@ -17,7 +17,7 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var config = builder.Configuration;
-const string DataSrc = "LAPTOP-8QVR89KA\\SQLEXPRESS02", Password = "12345";
+const string DataSrc = "MEOMATLON\\SQLEXPRESS", Password = "MukuroHoshimiya";
 
 
 // Add services to the container.
