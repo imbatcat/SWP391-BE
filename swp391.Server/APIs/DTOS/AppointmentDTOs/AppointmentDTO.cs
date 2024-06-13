ec7491af5f91f968dfd1096f24b1ba194b3cdf4d
Dixeliare <daoxuanlong492004@gmail.com>
2024-06-13 16:08:03 +0700

    ServicePayment

---
+++ swp391.Server/APIs/DTOS/AppointmentDTOs/AppointmentDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace PetHealthcare.Server.APIs.DTOS.AppointmentDTOs
+{
+    public class AppointmentDTO
+    {
+        public DateOnly AppointmentDate { get; set; }
+        public string AppointmentType { get; set; }
+        public string? AppointmentNotes { get; set; }
+        public double BookingPrice { get; set; }
+        public string PetId { get; set; }
+        public string AccountId { get; set; }
+        public string VeterinarianAccountId { get; set; }
+
+        public int TimeSlotId { get; set; }
+
+    }
+}
