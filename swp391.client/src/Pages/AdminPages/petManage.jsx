b193ce219d3563ee4a0c03bc43bed255262f89c7
MinhLuong30 <minhltse182480@fpt.edu.vn>
2024-06-14 23:27:51 +0700

    Update petManage.jsx

--- swp391.client/src/Pages/AdminPages/petManage.jsx
+++ swp391.client/src/Pages/AdminPages/petManage.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
-import { MDBBadge, MDBBtn, MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit';
+import { MDBBadge, MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit';
 import SideNav from '../../Component/SideNav/SideNav';
 
 function AdminPet() {
   const [pets, setPets] = useState([]);
-  const [accounts, setAccounts] = useState([]);
   const [searchInput, setSearchInput] = useState('');
   const [filteredPets, setFilteredPets] = useState([]);
 
