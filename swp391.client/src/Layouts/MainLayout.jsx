7aa4a0783f0be60a31df572bcfa3eb1697faf1be
Tom <ptdat04@gmail.com>
2024-06-20 10:53:35 +0700

    Minor pathing fixes and code refactoring

--- swp391.client/src/Layouts/MainLayout.jsx
+++ swp391.client/src/Layouts/MainLayout.jsx
@@ -1,3 +1,6 @@
+import {
+    MDBModal,
+} from 'mdb-react-ui-kit';
 import Footer from "../Component/Footer/Footer";
 import NavBar2 from "../Component/NavBar/NavBar2";
 import '../Layouts/MainLayout.css';
@@ -10,27 +13,6 @@ function MainLayout({ children }) {
     const [basicModal, setBasicModal] = useState(false);
 
     const toggleOpen = () => setBasicModal(!basicModal);
-    const getProfile = (token) => {
-        console.log(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`);
-        fetch(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                Accept: 'application/json'
-            }
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json(); // Parses JSON response into native JavaScript objects
-            })
-            .then(data => {
-                console.log(data); // Assuming setProfile is a function to update your component's state with the user profile
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-            });
-    };
     return (
         <div>
 
@@ -39,6 +21,15 @@ function MainLayout({ children }) {
             <main>
                 {children}
             </main>
+            <div className="fixed-bottom-right">
+                <button className="static-button" data-tooltip-id="add-button" onClick={toggleOpen}>+</button>
+                <Tooltip id='add-button' content={'Make An Appointment'}></Tooltip>
+
+                <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex='-1'>
+                    <SelectModal toggleOpen={toggleOpen}>
+                    </SelectModal>
+                </MDBModal>
+            </div>
             <Footer></Footer>
         </div>
     );
